f78e2b6b5537ff6f3a5468f536a9dace36e22bff
David Newsham <david.g.newsham@gmail.com>
2021-03-04 14:31:51 -0500

    Words and stuff

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('oi, I heard you like Vim'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
